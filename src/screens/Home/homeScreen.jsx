bac6714cb7ed2e0663e160cd9208838257547d73
mustaphamajed <mustapha.majed@esprit.tn>
2024-06-14 23:56:20 +0100

    refactor(home-screen): remove vertical scroll

--- src/screens/Home/homeScreen.jsx
+++ src/screens/Home/homeScreen.jsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from 'react'
-import { View, Text, FlatList, TextInput, StyleSheet, Image, ActivityIndicator } from 'react-native'
-import { fetchMovies } from '../../service/omdbService';
+import React from 'react'
+import { View, FlatList, StyleSheet, ActivityIndicator } from 'react-native'
 import useMovies from '../../hooks/useMovies';
 import MovieCard from '../../components/cards/movieCard';
 import { colors } from '../../helpers/colors';
@@ -11,35 +10,34 @@ import { ROUTE_NAMES } from '../../helpers/routes';
 const HomeScreen = () => {
     const navigation = useNavigation()
     const { movies, loading, refreshing, hasMore, searchTerm, setSearchTerm, loadMoreMovies, refreshMovies } = useMovies();
-  return (
-      <View style={styles.container}>
-          <View style={{ padding: 16}}>
-              
-    <SearchInput searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
-          </View>
-          <FlatList
-              style={{paddingHorizontal:16}}
-        data={movies}
-        keyExtractor={(item) => item.imdbID}
-        renderItem={({ item }) => (
-        <MovieCard item={item} onPress={() => navigation.navigate(ROUTE_NAMES.DETAILS, { id: item.imdbID })} />
-        )}
-        onEndReached={loadMoreMovies}
-        onEndReachedThreshold={0.5}
-        ListFooterComponent={loading && <ActivityIndicator size="large" color={colors.blue} />}
-        refreshing={refreshing}
-        onRefresh={refreshMovies}
-      />
-  </View>
-  )
+    return (
+        <View style={styles.container}>
+            <View style={{ padding: 16 }}>
+
+                <SearchInput searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+            </View>
+            <FlatList
+                style={{ paddingHorizontal: 16 }}
+                data={movies}
+                keyExtractor={(item) => item.imdbID}
+                showsVerticalScrollIndicator={false}
+                renderItem={({ item }) => (
+                    <MovieCard item={item} onPress={() => navigation.navigate(ROUTE_NAMES.DETAILS, { id: item.imdbID })} />
+                )}
+                onEndReached={loadMoreMovies}
+                onEndReachedThreshold={0.5}
+                ListFooterComponent={loading && <ActivityIndicator size="large" color={colors.blue} />}
+                refreshing={refreshing}
+                onRefresh={refreshMovies}
+            />
+        </View>
+    )
 }
 
 export default HomeScreen
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor:colors.white
+        flex: 1,
+        backgroundColor: colors.white
     },
-    
-   
-  });
+});
