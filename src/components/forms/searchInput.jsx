8ad940c4c093771baa7d44d7fe747f9cf579dff8
mustaphamajed <mustapha.majed@esprit.tn>
2024-06-14 14:27:59 +0100

    feat(components): create custom search input

---
+++ src/components/forms/searchInput.jsx
@@ -0,0 +1,27 @@
+import { StyleSheet, Text, TextInput, View } from 'react-native'
+import React from 'react'
+import { colors } from '../../helpers/colors'
+
+const SearchInput = ({searchTerm,setSearchTerm}) => {
+  return (
+ <TextInput
+        style={styles.searchInput}
+        placeholder="Search movies..."
+        value={searchTerm}
+        onChangeText={setSearchTerm}
+      />
+  )
+}
+
+export default SearchInput
+
+const styles = StyleSheet.create({
+    searchInput: {
+        height: 40,
+        borderColor: colors.gray62,
+        borderWidth: 1,
+        marginBottom: 16,
+        paddingHorizontal: 8,
+        borderRadius:8
+      },
+})
