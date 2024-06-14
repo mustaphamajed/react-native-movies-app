166868996cf2f86c2df4fa47db846bad57cbb5b9
mustaphamajed <mustapha.majed@esprit.tn>
2024-06-14 14:27:25 +0100

    feat(colors): add colors

---
+++ src/helpers/colors.js
@@ -0,0 +1,11 @@
+export const colors = {
+    white: '#FFFFFF',
+    gray62: '#9E9E9E',
+    darkText: '#212121',
+    blue: '#2F80ED',
+    navyBlue: '#0a065e70',
+    blue1F: '#1F1D3F',
+    gray9E:'#9E9E9E',
+    bgDarkColor: '#12141D',
+    
+  };
